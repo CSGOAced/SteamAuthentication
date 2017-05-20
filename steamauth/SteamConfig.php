363093646ea4ce888449d276456fb666dedffb24
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-05-20 21:23:43 +0100

    Fix Syntax Error

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -1,7 +1,7 @@
 <?php
 //Version 3.2
 $steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = $Var->FriendlyURL; // The main URL of your website displayed in the login page
+$steamauth['domainname'] = $Vars->FriendlyURL; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = $Link->Website; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = $Link->Website; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
