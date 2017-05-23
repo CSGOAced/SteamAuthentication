7817becd1a80b3686e4c35721f4f658c940d3bf6
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-05-23 14:41:04 +0100

    Fixed Syntax Error

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -6,7 +6,7 @@ $steamauth['logoutpage'] = $Link->Website; // Page to redirect to after a succes
 $steamauth['loginpage'] = $Link->Website; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
-if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!<br>Find this in steamauth/SteamConfig.php, Find the '<b>\$steamauth['api$
+if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!<br>Find this in steamauth/SteamConfig.php, Find the '<b>\$steamauth['apiapikey']</b>' Array. </div>");}
 if (empty($steamauth['domainname'])) {$steamauth['domainname'] = $_SERVER['SERVER_NAME'];}
 if (empty($steamauth['logoutpage'])) {$steamauth['logoutpage'] = $_SERVER['PHP_SELF'];}
 if (empty($steamauth['loginpage'])) {$steamauth['loginpage'] = $_SERVER['PHP_SELF'];}
